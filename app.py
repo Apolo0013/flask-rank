b3c037721c1719cd0fab3ed0fa51d852ce6658fd
Apolo0013 <148099939+Apolo0013@users.noreply.github.com>
2025-07-16 12:08:38 -0300

    push code

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/rank/get', methods=['POST', 'GET'])
+def PegarPessoalDosRanko():
+    # - 1 retorna uma lista com todos no rank
+        # Top: todos
+        # Top: 10
+        # Top: 5
+    pass
+
+
+@app.route('/rank/add', methods=['POStT'])
+def AddAPessoalDB():
+    # - 1 add ele em um json simple
+    # - 2 verificar se ele ja esta no banco de dados
+    pass
